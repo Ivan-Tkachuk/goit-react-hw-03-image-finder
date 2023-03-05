e8fcca5deca389d703c828bcd122862327ba8c6c
Ivan-Tkachuk <tkachuchella@gmail.com>
2023-03-05 18:59:57 +0200

    goit-react-hw-03-image-finder

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React from 'react';
+import { toast } from 'react-toastify';
+import { Header, Form, Field, Button, ButtonLabel } from './Searchbar.styled';
+import PropTypes from 'prop-types';
 
 export default class Searchbar extends React.Component {
-    render() {
-        return (
-            <header className="searchbar">
-            <form className="form">
-              <button type="submit" className="button">
-                <span className="button-label">Search</span>
-              </button>
-          
-              <input
-                className="input"
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search images and photos"
-              />
-            </form>
-          </header>
-        )
-    }
-}
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
 
+  state = {
+    searchName: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({
+      searchName: event.currentTarget.value.toLowerCase(),
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.searchName.trim() === '') {
+      return toast.error('Enter the search name please');
+    }
+    this.props.onSubmit(this.state.searchName);
+
+    this.setState({
+      searchName: '',
+    });
+  };
+
+  render() {
+    return (
+      <Header>
+        <Form onSubmit={this.handleSubmit}>
+          <Button type="submit">
+            <ButtonLabel>Search</ButtonLabel>
+          </Button>
+
+          <Field
+            name="searchName"
+            value={this.state.searchName}
+            onChange={this.handleNameChange}
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </Form>
+      </Header>
+    );
+  }
+}
